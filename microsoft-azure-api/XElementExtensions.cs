8c4a2cf4a66eebf3c74dd0a510126fd886250f06
wforney <wforney@hotmail.com>
2012-02-25 00:55:54 -0800

    Updated ignore list, solution, and added XElementExtensions.cs

---
+++ microsoft-azure-api/XElementExtensions.cs
@@ -0,0 +1,38 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="XElementExtensions.cs" company="improvGroup, LLC">
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Microsoft.WindowsAzure
+{
+    using System.Xml.Linq;
+
+    /// <summary>
+    /// The XElement extensions.
+    /// </summary>
+    public static class XElementExtensions
+    {
+        /// <summary>
+        /// Values the or default.
+        /// </summary>
+        /// <param name="xml">The XML.</param>
+        /// <returns></returns>
+        /// <remarks></remarks>
+        public static string ValueOrDefault(this XElement xml)
+        {
+            return xml == null ? null : xml.Value;
+        }
+
+        /// <summary>
+        /// Values the or default.
+        /// </summary>
+        /// <param name="xml">The XML.</param>
+        /// <param name="defaultValue">The default value.</param>
+        /// <returns></returns>
+        /// <remarks></remarks>
+        public static string ValueOrDefault(this XElement xml, string defaultValue)
+        {
+            return xml == null ? defaultValue : xml.Value;
+        }
+    }
+}
